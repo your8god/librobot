88fc004de4e8af851e132f5d6b1a9905feaed25b
Bogdan <bogdan.terebin@ya.ru>
2023-08-25 19:12:03 +0400

    fix for gcc and g++

--- librobot.cpp
+++ librobot.cpp
@@ -25,14 +25,14 @@ void Robot2D<T>::setMotion(const mathStruct::Point2D<T>& motion)
 	A[0][2] = motion.x;
 	A[1][2] = motion.y;
 
-	std::vector<T> B {m_point.x, m_point.y, T(1)};
+	std::vector<T> B {Robot2D<T>::m_point.x, Robot2D<T>::m_point.y, T(1)};
 	std::vector<T> C(size, T(0));
 
 	for (std::size_t i = 0; i < size; i++)
 		for (std::size_t j = 0; j < size; j++)
 			C[i] += A[i][j] * B[j];
 
-	m_point = mathStruct::Point2D<T>{C[0], C[1]};
+	Robot2D<T>::m_point = mathStruct::Point2D<T>{C[0], C[1]};
 }
 
 template<typename T>
@@ -44,14 +44,14 @@ void Robot3D<T>::setMotion(const mathStruct::Point3D<T>& motion)
 	A[1][3] = motion.y;
 	A[2][3] = motion.z;
 
-	std::vector<T> B {m_point.x, m_point.y, m_point.z, T(1)};
+	std::vector<T> B {Robot3D<T>::m_point.x, Robot3D<T>::m_point.y, Robot3D<T>::m_point.z, T(1)};
 	std::vector<T> C(size, T(0));
 
 	for (std::size_t i = 0; i < size; i++)
 		for (std::size_t j = 0; j < size; j++)
 			C[i] += A[i][j] * B[j];
 
-	m_point = mathStruct::Point3D<T>{C[0], C[1], C[2]};
+	Robot3D<T>::m_point = mathStruct::Point3D<T>{C[0], C[1], C[2]};
 }
 
 template class Robot2D<int>;
